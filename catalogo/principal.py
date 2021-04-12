d6ca38c1905f96caaf2c58bf6d5d64b3e09d4545
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2021-04-11 19:34:22 -0500

    Agregado catalogo

---
+++ catalogo/principal.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from fabricas import *
+from random import choice
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    fabricas = [FabricaHumanos(), FabricaOrcos()]
+    fabrica = choice(fabricas)
+    arma = fabrica.crear_arma()
+    escudo = fabrica.crear_escudo()
+
+    productos = []
+
+    productos.append(arma)
+    productos.append(escudo)
+
+    return render_template("productos.html", productos = productos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ catalogo/productos.py
@@ -0,0 +1,39 @@
+class Producto:
+    def __init__(self):
+        self.imagen = ""
+        self.descripcion = ""
+        self.grupo = ""
+
+class Arma(Producto):
+    def __init__(self):
+        Producto.__init__(self)
+
+class ArmaHumanos(Arma):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/arma.png"
+        self.descripcion = "Arma de los humanos"
+
+class ArmaOrcos(Arma):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/arma.png"
+        self.descripcion = "Arma de los orcos"
+
+class Escudo(Producto):
+    def __init__(self):
+        Producto.__init__(self)
+
+class EscudoHumanos(Escudo):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/escudo.png"
+        self.descripcion = "Escudo de los humanos"
+
+class EscudoOrcos(Escudo):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/escudo.png"
+        self.descripcion = "Escudo de los orcos"
+
+
